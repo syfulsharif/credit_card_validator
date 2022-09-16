b06048e4858532324be66b57d10b216cf56d3e06
syfulsharif <sharif.syful@gmail.com>
2022-09-17 00:25:42 +0600

    Tests being updated

--- card_validator/validator.py
+++ card_validator/validator.py
@@ -3,5 +3,5 @@ def get_issuer(number: str) -> str:
         return "Visa"
     if number.startswith("3"):
         return "American Express"
-    if number.startswith("5"):
+    if number.startswith("51") or number.startswith("52") or number.startswith("53") or number.startswith("54") or number.startswith("55"):
         return "Mastercard"
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -3,3 +3,12 @@ from card_validator.validator import get_issuer
 
 def test_get_issuer_visa():
     assert get_issuer("4343 4123 1435 1231") == "Visa"
+
+
+def test_get_issuer_master_card():
+    assert get_issuer("5142 2131 5464 6523") == "Mastercard"
+    assert not get_issuer("5642 2131 5464 6523") == "Mastercard"
+
+
+def test_get_issuer_american_express():
+    assert get_issuer("3343 4123 1435 1231") == "American Express"
